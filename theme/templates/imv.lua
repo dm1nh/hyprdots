34729c7e5e712dce0ef740ebe40341e7830f0479
dm1nh <dangminhngo.dev@gmail.com>
2024-11-01 15:38:28 +0700

    feat: update new fonts

--- theme/templates/imv.lua
+++ theme/templates/imv.lua
@@ -21,7 +21,7 @@ overlay = true
 overlay_text_color = ${white1}
 overlay_background_color = ${dark2}
 overlay_background_alpha = ff
-overlay_font = Galax Nerd Font Medium:11
+overlay_font = Galax Nerd Font Regular:16
 overlay_position_bottom = false
 
 # bindings
