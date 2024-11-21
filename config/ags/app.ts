4bcdb00f59eb60aef17eface81048154b31068f8
dm1nh <dangminhngo.dev@gmail.com>
2024-11-21 12:47:52 +0700

    feat: add ags notifications widget

--- config/ags/app.ts
+++ config/ags/app.ts
@@ -1,10 +1,12 @@
 import { App } from "astal/gtk3";
 import style from "./styles/style.scss";
 import Bar from "./widget/Bar";
+import NotificationPopups from "./widget/NotificationPopups";
 
 App.start({
   css: style,
   main() {
     App.get_monitors().map(Bar);
+    App.get_monitors().map(NotificationPopups);
   },
 });
